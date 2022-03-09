973b7405a2acba034852a5b37716ba21e98fcfaf
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2022-03-09 18:38:06 +0100

    parsing messages into 512-bits blocks

--- sha_256/src/lib.rs
+++ sha_256/src/lib.rs
@@ -24,6 +24,19 @@ fn pre_process(mut msg: Vec<u8>) -> Vec<u8> {
     msg
 }
 
+fn parse_block<'a>(msg: &'a Vec<u8>, index: usize) -> Result<&'a [u8], &'static str> {
+    let nb_blocks = msg.len() / 64;
+
+    if index > nb_blocks {
+        return Err("index is greater than the number of 512-bits blocks");
+    }
+
+    let start = ((512 * index) / 8) as usize;
+    let end = ((start + 512) / 8) as usize;
+
+    Ok(&msg[start..end])
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -43,5 +56,40 @@ mod tests {
         ];
 
         assert_eq!(msg, pre_processed_bytes);
+    }
+
+    #[test]
+    fn parse_block_valid() {
+        let raw_msg = String::from("hi");
+
+        let msg_bytes = raw_msg.as_bytes().to_vec();
+        let msg = pre_process(msg_bytes);
+
+        let block = match parse_block(&msg, 0) {
+            Ok(block) => block,
+            Err(err) => panic!("{err}"),
+        };
+
+        let valid_block: Vec<u8> = vec![
+            104, 105, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 16,
+        ];
+
+        assert_eq!(block, valid_block);
+    }
+
+    #[test]
+    #[should_panic(expected = "index is greater than the number of 512-bits blocks")]
+    fn parse_block_invalid() {
+        let raw_msg = String::from("hi");
+
+        let msg_bytes = raw_msg.as_bytes().to_vec();
+        let msg = pre_process(msg_bytes);
+
+        let _block = match parse_block(&msg, 65) {
+            Ok(block) => block,
+            Err(err) => panic!("{err}"),
+        };
     }
 }
